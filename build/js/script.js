ae7eb51d1f500666c162e3f374072f2248b6cf76
Di Coderr <dicoderr@gmail.com>
2020-04-22 22:27:42 +0300

    Update build

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,15 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+"use strict";
+var pageHeader = document.querySelector(".page-header");
+var headerToggle = document.querySelector(".page-header__toggle");
 
-pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove("page-header--nojs");
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+headerToggle.addEventListener("click", function () {
+  if (pageHeader.classList.contains("page-header--closed")) {
+    pageHeader.classList.remove("page-header--closed");
+    pageHeader.classList.add("page-header--opened");
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add("page-header--closed");
+    pageHeader.classList.remove("page-header--opened");
   }
 });
