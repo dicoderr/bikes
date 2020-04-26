1e57120c55b4731646e88cc961d3882df2d54257
Di Coderr <dicoderr@gmail.com>
2020-04-26 11:44:43 +0300

    Add footer

--- source/js/script.js
+++ source/js/script.js
@@ -5,3 +5,15 @@ var navbar = document.querySelector('.navbar');
 burger.addEventListener('click', function () {
   navbar.classList.toggle('shown');
 });
+
+var map; // eslint-disable-line
+
+function init() {
+  map = new ymaps.Map('map', {
+    center: [59.938635, 30.323118],
+    zoom: 15,
+    controls: []
+  });
+}
+
+ymaps.ready(init);
